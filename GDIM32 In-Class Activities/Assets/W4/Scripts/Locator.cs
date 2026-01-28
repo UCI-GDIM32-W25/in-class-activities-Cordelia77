eab75396abd16b30c8e99b6a7a9fdd14621a0c6d
Cordelia77 <yuxid15@uci.edu>
2026-01-28 10:57:18 +0800

    1111

---
+++ GDIM32 In-Class Activities/Assets/W4/Scripts/Locator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Locator : MonoBehaviour
+{
+    public static Locator Instance { get; private set; }
+    public W4Pigeon W4Pigeon { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        W4Pigeon = playerObj.GetComponent<W4Pigeon>();
+    }
+}
